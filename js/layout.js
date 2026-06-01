7ffcb9b5580c603c243167cae90a373ea673d3d3
ArmandoMendez86 <desarrolloweb150305@gmail.com>
2026-05-31 19:30:02 -0600

    Terminado, personalizando

--- js/layout.js
+++ js/layout.js
@@ -1,5 +1,10 @@
 (function () {
-  const MAP_EMBED = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2909.967460058678!2d-89.24859!3d43.1682072!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x63fdab021c4c42e7%3A0x851d662928bf3d8e!2sMadtown%20Maids!5e0!3m2!1sen!2sus!4v1770946982900!5m2!1sen!2sus';
+  const FOOTER_ADDRESS_LINE1 = "450 B Paradise Rd 162";
+  const FOOTER_ADDRESS_LINE2 = "Swampscott, MA 01907";
+  const FOOTER_PHONE_DISPLAY = "(617) 686-6805";
+  const FOOTER_PHONE_TEL = "+16176866805";
+  const MAP_EMBED =
+    "https://www.google.com/maps?q=450+B+Paradise+Rd+162+Swampscott,+MA+01907&hl=en&z=15&output=embed";
 
   function getBasePath() {
     const path = window.location.pathname.replace(/\\/g, '/');
@@ -62,11 +67,11 @@
             <ul class="footer__contact">
               <li>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"/><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"/></svg>
-                <span>1850 Summerfield Way Apt 3318<br>Sun Prairie, WI 53590</span>
+                <span>${FOOTER_ADDRESS_LINE1}<br>${FOOTER_ADDRESS_LINE2}</span>
               </li>
               <li>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"/></svg>
-                <a href="tel:+16085713484">(608) 571-3484</a>
+                <a href="tel:${FOOTER_PHONE_TEL}">${FOOTER_PHONE_DISPLAY}</a>
               </li>
               <li>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/></svg>
@@ -78,7 +83,7 @@
               </li>
             </ul>
             <div class="footer__map">
-              <iframe src="${MAP_EMBED}" width="100%" height="100%" style="border:0" allowfullscreen loading="lazy" referrerpolicy="no-referrer-when-downgrade" title="Madtown Maids - Service Area"></iframe>
+              <iframe src="${MAP_EMBED}" width="100%" height="100%" style="border:0" allowfullscreen loading="lazy" referrerpolicy="no-referrer-when-downgrade" title="Madtown Maids - ${FOOTER_ADDRESS_LINE2}"></iframe>
             </div>
           </div>
         </div>
